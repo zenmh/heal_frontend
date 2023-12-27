dbe13675197ce47a0a0ac1078bc8b28acd6a415a
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-27 20:30:57 +0600

    Setup the theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
   return (
     <div>
       <h2 className="text-green-500 text-3xl font-bold">
         Hello World. I&apos;m Comming!
       </h2>
+      <Button className="m-10">Click Me</Button>
     </div>
   );
 };
