a27f6f974cbfc29dd75d19494c89760f07f22874
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-31 18:58:35 +0600

    Created global not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold text-rose-500 text-center">
+        Page Not Found !!
+      </h2>
+    </div>
+  );
+};
+
+export default NotFound;
