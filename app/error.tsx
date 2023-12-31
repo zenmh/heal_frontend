c9d595ea6f80bd50ea611959b58d225c89bdcf47
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-31 19:03:17 +0600

    Created global Error page to show error message

---
+++ app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <p className="text-red-400 text-center mt-20">Something went wront !!</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
