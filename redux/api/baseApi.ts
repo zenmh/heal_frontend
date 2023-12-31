28adf6a50e197f21664cc30ce3058087378f87a4
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-31 19:45:51 +0600

    Connect axiso base query helper with redux base api

--- redux/api/baseApi.ts
+++ redux/api/baseApi.ts
@@ -1,9 +1,10 @@
+import axiosBaseQuery from "@/helpers/axios/axiosBaseQuery";
 import { getBaseUrl } from "@/helpers/envConfig";
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: getBaseUrl() }),
+  baseQuery: axiosBaseQuery({ baseUrl: getBaseUrl() }),
   endpoints: () => ({}),
   tagTypes: ["user"],
 });
