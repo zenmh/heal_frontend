1990cfc3d9f0398b66cbe3f04f0fbb15085c3021
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-31 18:48:29 +0600

    Create root reducer file and add type for root state and app dispatch in store.ts

--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,9 @@
+import reducer from "./rootReducer";
 import { configureStore } from "@reduxjs/toolkit";
 
-const store = configureStore({ reducer: {} });
+const store = configureStore({ reducer });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
