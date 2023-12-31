937c2a685f087e1ad15070d8ac5ff3e4dc354142
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-12-31 18:56:21 +0600

    Wrap the app layout by providers

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
-import { Navbar } from "@/components/shared/navbar";
+import Providers from "@/lib/Providers";
 import { Footer } from "@/components/shared";
+import { Navbar } from "@/components/shared/navbar";
+import { Inter as FontSans } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Heal",
@@ -18,20 +19,22 @@ export const fontSans = FontSans({
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <head />
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased max-w-[1444px] mx-auto",
-          fontSans.variable
-        )}
-      >
-        <header>
-          <Navbar />
-        </header>
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <Providers>
+      <html lang="en">
+        <head />
+        <body
+          className={cn(
+            "min-h-screen bg-background font-sans antialiased max-w-[1444px] mx-auto",
+            fontSans.variable
+          )}
+        >
+          <header>
+            <Navbar />
+          </header>
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </Providers>
   );
 }
