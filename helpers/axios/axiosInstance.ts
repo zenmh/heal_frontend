43044f2d26cdb3cffc33e43378c1d21d1c1775a0
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-01 16:32:50 +0600

    Handle send accessToken with api call by axios in instance

--- helpers/axios/axiosInstance.ts
+++ helpers/axios/axiosInstance.ts
@@ -1,3 +1,5 @@
+import { authKey } from "@/constants/storageKeys";
+import { getFromLocalStorage } from "@/utils/localStorage";
 import axios from "axios";
 
 const instance = axios.create();
@@ -5,5 +7,18 @@ const instance = axios.create();
 instance.defaults.headers.post["Content-Type"] = "application/json";
 instance.defaults.headers["Accept"] = "application/json";
 instance.defaults.timeout = 60000;
+
+instance.interceptors.request.use(
+  function (config) {
+    const accessToken = getFromLocalStorage(authKey);
+
+    if (accessToken) config.headers.Authorization = accessToken;
+
+    return config;
+  },
+  function (err: any) {
+    return Promise.reject(err);
+  }
+);
 
 export { instance };
