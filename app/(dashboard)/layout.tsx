6cc2d58a53d0bd568775ad040ec9bebe9d4f7744
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-01 15:30:59 +0600

    Handle is user logged in or not to handle protected routes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,30 @@
 "use client";
 
-import { ReactNode } from "react";
+import { isLoggedIn } from "@/services/authService";
+import { useRouter } from "next/navigation";
+import { ReactNode, useEffect, useState } from "react";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const userLoggedIn = isLoggedIn();
+  const { push } = useRouter();
+
+  useEffect(() => {
+    if (!userLoggedIn) push("/auth");
+
+    setLoading(true);
+  }, [push, userLoggedIn, isLoading]);
+
+  if (!isLoading)
+    return (
+      <h3 className="text-green-400 text-4xl font-bold text-center">
+        Loading....
+      </h3>
+    );
+
   return (
     <div className="flex flex-row items-center justify-evenly">
-      <div>Sidebar</div>
+      <aside>Sidebar</aside>
       <div>{children}</div>
     </div>
   );
