989a1f2943eed68794348d2ff682d920df0ad638
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-09 17:29:05 +0600

    Made the sketch of the doctor details page

--- app/doctors/[id]/page.tsx
+++ app/doctors/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { FC } from "react";
 
 interface DoctorDetailsProps {
@@ -7,7 +8,15 @@ interface DoctorDetailsProps {
 }
 
 const DoctorDetails: FC<DoctorDetailsProps> = ({ params }) => {
-  return <div>doctor details</div>;
+  return (
+    <section className="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
+      <div className="lg:col-span-2 md:col-span-2 border-2 border-sky-400">
+        <div className="border-2 border-primary mb-4">]</div>
+        <div className="border-2 border-primary"></div>
+      </div>
+      <div className="lg:col-span-1 md:col-span-2 border-2 border-primary"></div>
+    </section>
+  );
 };
 
 export default DoctorDetails;
