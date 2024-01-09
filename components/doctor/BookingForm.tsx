852049794338791a498f8f8498a90bce58af2434
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-10 01:20:52 +0600

    Installed the command component of shadcn ui

--- components/doctor/BookingForm.tsx
+++ components/doctor/BookingForm.tsx
@@ -80,14 +80,9 @@ const BookingForm = () => {
                     mode="single"
                     selected={field.value}
                     onSelect={field.onChange}
-                    disabled={
-                      (date) => date < new Date()
-                      //     || date < new Date("1900-01-01")
-                    }
+                    disabled={(date) => date < new Date()}
                     initialFocus
                   />
-                  {/* 2024-01-06T13:38:13.940Z        
-Mon Jan 01 2024 00:00:00 GMT+0600 */}
                 </PopoverContent>
               </Popover>
               <FormDescription>
@@ -97,6 +92,49 @@ Mon Jan 01 2024 00:00:00 GMT+0600 */}
             </FormItem>
           )}
         />
+        <FormField
+          control={form.control}
+          name="dob"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel>Date of birth</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) => date < new Date()}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Your date of birth is used to calculate your age.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
