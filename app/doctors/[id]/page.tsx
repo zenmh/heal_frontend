637e4c019a8237c15b94b10c3e1288552fe784cd
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-09 18:34:40 +0600

    Add tabs for show doctors about, location, contact and reviews

--- app/doctors/[id]/page.tsx
+++ app/doctors/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useGetDoctorQuery } from "@/redux/api/doctorApi";
 import { AtSign, MapPin, Phone, Star } from "lucide-react";
 import Image from "next/image";
@@ -16,8 +17,8 @@ const DoctorDetails: FC<DoctorDetailsProps> = ({ params: { id } }) => {
 
   return (
     <section className="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
-      <div className="lg:col-span-2 md:col-span-2 border-2 border-sky-400">
-        <div className="rounded-lg flex flex-col md:flex-row border-2 border-green-500">
+      <div className="lg:col-span-2 md:col-span-2">
+        <div className="rounded-lg flex flex-col md:flex-row items-center">
           <Image
             src={
               doctor?.data?.image
@@ -30,7 +31,7 @@ const DoctorDetails: FC<DoctorDetailsProps> = ({ params: { id } }) => {
             className="rounded-l-lg"
           />
           <div className="p-3">
-            <h2 className="text-2xl font-semibold text-[#00ACB1] mb-1">
+            <h2 className="text-2xl font-semibold text-primary mb-1">
               {doctor?.data?.name}
             </h2>
             <p className="mt-2 text-gray-400">{doctor?.data?.experiences}</p>
@@ -45,7 +46,7 @@ const DoctorDetails: FC<DoctorDetailsProps> = ({ params: { id } }) => {
                 {Array(5)
                   .fill(null)
                   .map((_, i: number) => (
-                    <Star key={i} fill="#FCB71B" color="#FCB71B" size={20} />
+                    <Star key={i} fill="gold" color="gold" size={20} />
                   ))}
               </span>
               <span className="text-gray-400 ml-4">(4/5)</span>
@@ -73,7 +74,48 @@ const DoctorDetails: FC<DoctorDetailsProps> = ({ params: { id } }) => {
             </div>
           </div>
         </div>
-        <div className="border-2 border-primary"></div>
+        <Tabs
+          defaultValue="about"
+          className="w-full border-2 border-green-600 rounded-lg mt-4"
+        >
+          <TabsList className="w-full py-6">
+            <TabsTrigger
+              value="about"
+              className="font-bold text-md border-2 border-gray-400 rounded-none w-full"
+            >
+              About
+            </TabsTrigger>
+            <TabsTrigger
+              value="location_and_contact"
+              className="font-bold text-md border-2 border-gray-400 border-l-0 rounded-none w-full"
+            >
+              Location & Contact
+            </TabsTrigger>
+            <TabsTrigger
+              value="reviews"
+              className="font-bold text-md border-2 border-gray-400 border-l-0 rounded-none w-full"
+            >
+              Reviews
+            </TabsTrigger>
+          </TabsList>
+          <div className="p-4">
+            <TabsContent value="about">About</TabsContent>
+            <TabsContent value="location_and_contact">
+              <p>
+                <span className="font-semibold">Branch:</span>&nbsp;
+                {doctor?.data?.branch.slice(0, 1)}
+                <span className="lowercase">
+                  {doctor?.data?.branch.slice(1)}
+                </span>
+              </p>
+              <p>
+                <span className="font-semibold">Contact:</span>&nbsp;
+                +880&nbsp;1{doctor?.data?.contactNo}
+              </p>
+            </TabsContent>
+            <TabsContent value="reviews">Reviews</TabsContent>
+          </div>
+        </Tabs>
       </div>
       <div className="lg:col-span-1 md:col-span-2 border-2 border-primary"></div>
     </section>
