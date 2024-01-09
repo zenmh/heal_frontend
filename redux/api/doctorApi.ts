bb64c42c8df9183f27a13b5d457d8f7e988bf834
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-09 17:50:48 +0600

    Fetch the single doctor data from backend by rtk query

--- redux/api/doctorApi.ts
+++ redux/api/doctorApi.ts
@@ -2,6 +2,13 @@ import baseApi from "./baseApi";
 
 const doctorApi = baseApi.injectEndpoints({
   endpoints: ({ query, mutation }) => ({
+    getDoctor: query({
+      query: (id) => ({
+        url: "/doctors",
+        method: "GET",
+        params: { id },
+      }),
+    }),
     getDoctors: query({
       query: (data) => ({
         url: "/doctors",
