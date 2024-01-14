608865a66711bf3ba97860137731ef4d81b1b393
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-15 00:57:48 +0600

    Create the featured service card

---
+++ components/home/Service.tsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const Service = () => {
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create project</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>Hello</CardContent>
+      <CardFooter className="flex justify-between">
+        <Button variant="outline">Cancel</Button>
+        <Button>Deploy</Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default Service;
