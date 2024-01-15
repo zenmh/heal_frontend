cc889c2b0c21774ac227044784388a84c877f08f
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2024-01-15 23:11:21 +0600

    Styled the desktop sidebar

--- components/shared/sidebar/DesktopSidebar.tsx
+++ components/shared/sidebar/DesktopSidebar.tsx
@@ -1,5 +1,11 @@
-const DesktopSidebar = () => {
-  return <aside>Desktopside bar</aside>;
+import { ReactNode } from "react";
+
+const DesktopSidebar = ({ children }: { children: ReactNode }) => {
+  return (
+    <aside className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
+      {children}
+    </aside>
+  );
 };
 
 export default DesktopSidebar;
